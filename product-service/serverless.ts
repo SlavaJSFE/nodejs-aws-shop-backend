21157a072a1576e97b83c38aae664bf4e10507fe
Viachaslau Shpileuski <viachaslau.shpileuski@coxautoinc.com>
2022-07-24 22:05:50 +0300

    feat: init task 4 implementation

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -20,6 +20,11 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      PG_HOST: process.env.PG_HOST,
+      PG_PORT: process.env.PG_PORT,
+      PG_DATABASE: process.env.PG_DATABASE,
+      PG_USERNAME: process.env.PG_USERNAME,
+      PG_PASSWORD: process.env.PG_PASSWORD
     },
   },
   functions: { hello, getProductsList, getProductsById },
