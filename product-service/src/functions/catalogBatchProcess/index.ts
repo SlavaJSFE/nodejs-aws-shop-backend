fe4c6351ee840e950568b4a7136af69837cae4ab
Viachaslau Shpileuski <viachaslau.shpileuski@coxautoinc.com>
2022-08-07 22:06:20 +0300

    feat: init task 6

---
+++ product-service/src/functions/catalogBatchProcess/index.ts
@@ -0,0 +1,13 @@
+import { handlerPath } from "@libs/handler-resolver";
+
+export default {
+  handler: `${handlerPath(__dirname)}/catalogBatchProcess.handler`,
+  events: [
+    {
+      sqs: {
+        batchSize: 5,
+        arn: ''
+      },
+    },
+  ],
+};
